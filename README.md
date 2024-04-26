bfbb3eaf943e3a7530bd6c17b0355c2672cb0e65
Igor Jakus <igorjakus@protonmail.com>
2024-04-26 16:22:38 +0200

    Add basic app structure and icon

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Let's consider a game of tic-tac-toe with fog of war.
 We have an ordinary tic-tac-toe board, two players and a game server. Players submit motion proposals to the game server (player A does not see player B's proposal and vice versa) 
 and the server either accepts this move (if the proposed field is empty), or informs the player that the move is not allowed, and the player has the right to try again (any number of times).
 Except that it's a normal tic-tac-toe (i.e. the goal is to arrange 3 of your characters in a row, column or on diagonal).
+
+
+###
+Icon by Erifqi Zetiawan[https://www.flaticon.com/free-icons/tic-tac-toe]
---
+++ main.py
@@ -0,0 +1,49 @@
+import pygame
+
+
+class App:
+    def __init__(self):
+        pygame.init()
+        self.running = True
+        self.window = None
+
+        icon = pygame.image.load("assets/icon.png")
+        pygame.display.set_icon(icon)
+        pygame.display.set_caption("TicTacToe - fog of war @igorjakus")
+
+    def on_event(self, event):
+        """Handles all events"""
+        if event.type == pygame.QUIT:
+            self.running = False
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        pass
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+
+class Window:
+    def __init__(self):
+        pass
+
+
+class Board:
+    def __init__(self):
+        self.board = [0] * 9
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
